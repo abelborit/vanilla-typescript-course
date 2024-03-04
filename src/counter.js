77d2c15cfda3ac2d2a08c50b476da74f3a99e8e4
Abel Borit <abelboritguitton@gmail.com>
2024-03-04 17:20:37 -0500

    [FEAT] transpilation of TypeScript to JavaScript using: "npx tsc src/counter.ts"

---
+++ src/counter.js
@@ -0,0 +1,16 @@
+/* esta sería la transpilación de TypeScript a JavaScript usando en la terminal: npx tsc src/counter.ts */
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.setupCounter = void 0;
+function setupCounter(element) {
+  var counter = 0;
+  var setCounter = function (count) {
+    counter = count;
+    element.innerHTML = "count is ".concat(counter);
+  };
+  element.addEventListener("click", function () {
+    return setCounter(counter + 1);
+  });
+  setCounter(0);
+}
+exports.setupCounter = setupCounter;
